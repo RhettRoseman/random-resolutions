dcee99754b4767ef407f1109cfac349a68b3b19a
Andrew Allen <allenkandrew@gmail.com>
2023-11-15 12:30:37 -0500

    login form styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,60 @@
-import LoginForm from "../forms/LoginForm";
+import React from 'react';
+import styled from 'styled-components';
 
-export default function Home() {
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const FormWrapper = styled.div`
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  width: 300px;
+  max-width: 80%;
+  margin-bottom: 500px;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  margin-bottom: 20px;
+  font-family: 'Poppins', sans-serif;
+`;
+
+const InputField = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 15px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-sizing: border-box;
+`;
+
+const LoginButton = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: #808080;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-family: 'Poppins', sans-serif;
+`;
+
+const LoginForm = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-     <LoginForm/>
-    </div>
+    <Container>
+      <FormWrapper>
+        <Title>Login</Title>
+        <InputField type="text" placeholder="Username" />
+        <InputField type="password" placeholder="Password" />
+        <LoginButton>Login</LoginButton>
+      </FormWrapper>
+    </Container>
   );
-}
+};
+
+export default LoginForm;
